b13777e08807498c7d976683d499b62754c0f0b6
Juan@SDSharp <juan@sdsharp.com>
2021-10-09 09:28:08 -0500

    Initial Commit

---
+++ Source/UltimateFPS/UltimateFPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UltimateFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULTIMATEFPS_API AUltimateFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
