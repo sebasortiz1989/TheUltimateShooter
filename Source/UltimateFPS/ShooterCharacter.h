f9d82011a7257ce9f1090a43c0e1f4e603e4333e
Juan <juan@sdsharp.com>
2021-10-24 11:39:51 -0500

    37. Refactor Beam End Code

--- Source/UltimateFPS/ShooterCharacter.h
+++ Source/UltimateFPS/ShooterCharacter.h
@@ -38,6 +38,8 @@ protected:
 	// Called when the fire button is pressed
 	void FireWeapon();
 
+	bool GetBeamEndLocation(const FVector& MuzzleSocketLocation, FVector& OutBeamLocation);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
